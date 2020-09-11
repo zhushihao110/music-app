bb0726ffbf501a8d503c6d420e692cab53a978f9
v_shihaozhu@tencent.com <v_shihaozhu@tencent.com>
2020-09-11 10:26:56 +0800

    add

--- src/api/request.js
+++ src/api/request.js
@@ -2,7 +2,7 @@
  * @Author: ZSH
  * @Date: 2020-08-31 11:14:10
  * @LastEditors: ZSH
- * @LastEditTime: 2020-08-31 11:47:07
+ * @LastEditTime: 2020-09-10 11:09:56
  */
 import { axiosInstance } from './config'
 
@@ -12,4 +12,12 @@ export const BannerListRequest = () => {
 
 export const getRecommendListRequest = () => {
   return axiosInstance.get ('/personalized');
+}
+
+export const getHotSingerListRequest = (count) => {
+  return axiosInstance.get(`/top/artists?offset=${count}`);
+}
+
+export const getSingerListRequest= (category, alpha, count) => {
+  return axiosInstance.get(`/artist/list?cat=${category}&initial=${alpha.toLowerCase()}&offset=${count}`);
 }
